de748df6d1cb9b10329b275e6189996877c2b8d4
MoizAli-96 <mapirani.96@gmail.com>
2018-03-07 02:41:30 -0500

    Sketch and PIXI added

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var canvas = document.getElementById('spec');
 var ctx = canvas.getContext('2d');
 ctx.canvas.width = document.documentElement.clientWidth;
 ctx.canvas.height = "50";
-
+var dataURL;
 
 // create an image object and get it’s source
 var imgg = new Image();
@@ -98,7 +98,7 @@ function setVal(mod) {
         $(".box" + num).css({
             "background-color": mod.id
         });
-        $(".box"+num).attr('id',mod.id);
+        $(".box" + num).attr('id', mod.id);
         $(".box" + num).toggleClass('clicked');
     }
     num++;
@@ -121,7 +121,7 @@ $('#btn1').click(function () {
     $(".box" + num).css({
         "background-color": ""
     });
-    $(".box"+num).attr('id', '');
+    $(".box" + num).attr('id', '');
     $(".box" + num).removeClass("clicked");
     num--;
 });
@@ -130,37 +130,180 @@ $('#btn3').click(function () {
     $(".box").css({
         "background-color": ""
     });
-    $(".box"+num).attr('id', '');
+    $(".box" + num).attr('id', '');
     $('.box').removeClass("clicked");
     num = 0;
 });
 
 canvas.addEventListener('click', pick);
 
+
 //Draw on big Canvas
 $('#btn4').click(function () {
     console.log('hi there');
-    var c = document.getElementById("Canvas");
+    $('.sketch').attr('id', 'myCanvas');
+    var c = document.getElementById("myCanvas");
+    console.log(c);
     var ctx = c.getContext("2d");
-    var h_wid = ctx.canvas.width / 2;
-    var h_hei = ctx.canvas.height / 2;
-    console.log('color');
-    var color = $(".box"+i).attr('id');
-    for(var i=1; i<num; i++){
-        var color = $(".box"+i).attr('id');
-        console.log($(".box"+i));
+    var h_wid = ctx.canvas.width / 4;
+    var h_hei = ctx.canvas.height / 4;
+
+    var color = $(".box" + i).attr('id');
+    for (var i = 1; i < num; i++) {
+        var color = $(".box" + i).attr('id');
+        console.log($(".box" + i));
         ctx.fillStyle = color;
-        if(i==1){
+        if (i == 1) {
             ctx.fillRect(0, 0, h_wid, h_hei);
         }
-        else if(i==2){
-            ctx.fillRect(h_wid, 0, h_wid+h_wid, h_hei);
+        else if (i == 2) {
+            ctx.fillRect(h_wid, 0, h_wid + h_wid, h_hei);
         }
-        else if(i==3){
-            ctx.fillRect(0, h_hei, h_wid, h_hei+h_hei);
+        else if (i == 3) {
+            ctx.fillRect(0, h_hei, h_wid, h_hei + h_hei);
         }
-        else if(i==4){
-            ctx.fillRect(h_wid, h_hei, h_wid+h_wid, h_hei+h_hei);
+        else if (i == 4) {
+            ctx.fillRect(h_wid, h_hei, h_wid + h_wid, h_hei + h_hei);
+        }
+    }
+    var dataURL = c.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    colorSetup();
+    //console.log(dataURL);
+
+});
+
+//sketch.js
+
+var COLOURS = [];
+
+function colorSetup() {
+    for (var i = 1; i < num; i++) {
+        var color = $(".box" + i).attr('id');
+        var colour = w3color(color);
+        colour = colour.toHexString();
+        COLOURS[i] = colour;
+        if(i > num){
+            color = $(".box"+num-1).attr('id');
+            var colour = w3color(color);
+            colour = colour.toHexString();
+            COLOURS[num] = colour;
+        }
+    }
+
+    for (var i = 1; i < num; i++) {
+        console.log('--' + COLOURS[i]);
+    }
+}
+
+var radius = 0;
+Sketch.create({
+    fullscreen: false,
+    height: 450,
+    width: 1225,
+    container: document.getElementById('canv'),
+    autoclear: false,
+    retina: 'auto',
+    setup: function () {
+        console.log('setup');
+    },
+    update: function () {
+        radius = 2 + abs(sin(this.millis * 0.003) * 50);
+    },
+    // Event handlers
+    keydown: function () {
+        if (this.keys.C) this.clear();
+    },
+    // Mouse & touch events are merged, so handling touch events by default
+    // and powering sketches using the touches array is recommended for easy
+    // scalability. If you only need to handle the mouse / desktop browsers,
+    // use the 0th touch element and you get wider device support for free.
+    touchmove: function () {
+        for (var i = this.touches.length - 1, touch; i >= 0; i--) {
+            touch = this.touches[i];
+            this.lineCap = 'round';
+            this.lineJoin = 'round';
+            this.fillStyle = this.strokeStyle = COLOURS[i % COLOURS.length];
+            this.lineWidth = radius;
+            this.beginPath();
+            this.moveTo(touch.ox, touch.oy);
+            this.lineTo(touch.x, touch.y);
+            this.stroke();
         }
     }
 });
+
+
+// pixi.js
+// import * as PIXI from 'pixi.js';
+
+// console.clear();
+
+// var app = new PIXI.Application(1225, 450, {
+//     autoStart: false,
+//     backgroundColor: 0x000000,
+//     view: myCanvas
+// });
+
+// var rt = [];
+// for (var i = 0; i < 3; i++) rt.push(PIXI.RenderTexture.create(app.screen.width, app.screen.height));
+// var current = 0;
+
+// var bg, brush, displacementFilter;
+
+// var container = new PIXI.Container();
+// app.stage.addChild(container);
+
+// app.loader.add('bg', dataURL);
+// app.loader.add('one', 'https://raw.githubusercontent.com/PavelLaptev/test-rep/master/dis-varOne.png');
+// app.loader.load(function (loader, resources) {
+//     var tempBg = new PIXI.Sprite(resources.bg.texture);
+//     tempBg.width = app.screen.width;
+//     tempBg.height = app.screen.height;
+
+//     app.renderer.render(tempBg, rt[0]);
+//     bg = new PIXI.Sprite(rt[0]);
+
+//     brush = new PIXI.Sprite(resources.one.texture);
+//     brush.anchor.set(0.5);
+//     displacementFilter = new PIXI.filters.DisplacementFilter(brush);
+//     container.filters = [displacementFilter];
+//     displacementFilter.scale.x = 10;
+//     displacementFilter.scale.y = 10;
+
+//     container.addChild(bg, brush);
+
+//     app.stage.interactive = true;
+
+//     app.stage.on('pointerdown', onPointerDown)
+//         .on('pointerup', onPointerUp)
+//         .on('pointermove', onPointerMove);
+
+//     app.start();
+// });
+
+// function snap(event) {
+//     app.renderer.render(app.stage, rt[2 - current]);
+//     bg.texture = rt[2 - current];
+//     current = 2 - current;
+// }
+
+// var dragging = false;
+
+// function onPointerDown(event) {
+//     dragging = true;
+//     onPointerMove(event);
+// }
+
+// function onPointerMove(event) {
+//     const x = event.data.global.x;
+//     const y = event.data.global.y;
+//     displacementFilter.scale.x = Math.atan(x - brush.x) * 4;
+//     displacementFilter.scale.y = Math.atan(y - brush.y) * 4;
+
+//     brush.position.copy(event.data.global);
+//     if (dragging) snap(event);
+// }
+
+// function onPointerUp() {
+//     dragging = false;
+// }
